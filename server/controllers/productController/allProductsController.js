4a53a90f6840c775053af877884bde4cf307c5ec
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-05-29 00:26:02 +0200

    filter

--- server/controllers/productController/allProductsController.js
+++ server/controllers/productController/allProductsController.js
@@ -1,7 +1,25 @@
+const mongoose = require('mongoose')
+const Products = require('../../models/productsModel')
 
+const allProductsController = async (req ,res)=>{
+  try{
 
-const allProductsController =()=>{
-
+    const {keyword,min_price,max_price,taxonomy_id} =req.body
+    let query ={};
+    keyword ? query.title = new RegExp(keyword,i) :null
+    min_price ? query.price ={['$gte'] : min_price} : null
+    max_price ? query.price ={['$gte'] : max_price} :null
+    min_price && max_price ? query.price ={['$gte'] : min_price ,['$gte'] : max_price} :null
+    taxonomy_id ?query.taxonomy_id = taxonomy_id : null
+   const products =await Products.find(query).limit(20)
+   
+   res.status(200).json({
+       message : 'sucess',
+       data: products
+   })
+  }catch(err)  {
+ res.send(err.message)
+  }
 
 
 }
