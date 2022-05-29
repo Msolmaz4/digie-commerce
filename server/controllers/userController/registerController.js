61a4062f1056093554d135ce3335e89a8386c2c1
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-05-29 18:35:08 +0200

    register

--- server/controllers/userController/registerController.js
+++ server/controllers/userController/registerController.js
@@ -1,7 +1,24 @@
+const Users = require('../../models/productsModel')
+const bcrypt = require('bcrypt')
 
 
+const registerController = async (req,res)=>{
+    try{
+        const { email,password,name,surname} = req.bod
+        if(!email || !password || !name || surname ) return res.send('please fiil inre')
+        const emailControl =await Users.findOne({email:email})
+        if(emailControl) return res.send('this user is already ')
+        const newUser = await new Users({
+            name:name,
+            surname:surname,
+            email:email,
+            password:bcrypt.hashSync(password,8)
+        })
 
-const registerController =()=>{
+    }catch(err){
+        console.log(err.message)
+        res.send(err.message)
+    }
 
 }
 module.exports = registerController
