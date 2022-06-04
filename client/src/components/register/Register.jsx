6ff7534e6ee8f3bce5d02c7d2c5ea7f28627b724
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-06-04 23:27:22 +0200

    syles

---
+++ client/src/components/register/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Register.module.css'
+
+export default function register() {
+  return (
+    <div>
+        <div className={styles.contanier}></div>
+        <div className={styles.imgContanier}></div>
+        <div className={styles.formContanier}></div>
+    </div>
+  )
+}
