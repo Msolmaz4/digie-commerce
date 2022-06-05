7171f6c92536a56d9cf445871d8f0a8cd1b7c629
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-06-05 22:08:36 +0200

    register

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import styles from './Register.module.css'
-
+import React from "react";
+import styles from "./Register.module.css";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faUserAlt,faMailBulk} from '@fortawesome/free-solid-svg-icons'
+//get stardet react fontaweso v5
 export default function register() {
+
+const handleInputChange = ()=>{
+    
+}
+
   return (
     <div>
-        <div className={styles.contanier}></div>
-        <div className={styles.imgContanier}></div>
-        <div className={styles.formContanier}></div>
+      <div className={styles.contanier}>
+        <div className={styles.imgContanier}>
+          <img src="/3.jpg" alt="login" className={styles.img} />
+        </div>
+        <div className={styles.formContanier}>
+            <form>
+                <div className={styles.formTitle}> 
+                <h1 className={styles.title}>Register </h1>
+                </div>
+                <div className={styles.formInput}>
+                    <div className={styles.inputContanier}>
+                      <FontAwesomeIcon icon={faUserAlt} className={styles.icons} />
+                      <input className={styles.input} type='text' name="name" onChange={handleInputChange} />
+                    </div>
+                </div>
+            </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
